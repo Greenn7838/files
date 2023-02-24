922f617b4672fe2373e00f755f248e0ce8dd58f1
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-02-24 17:28:14 +0700

    upload

--- check.bat
+++ check.bat
@@ -2,7 +2,6 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > out.txt 2>&1
 del /f "C:\Users\Public\Desktop\Firefox.lnk" > out.txt 2>&1
-md "C:\Users\Public\Desktop\butter" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user Administrator Tunghs123 /add >nul
@@ -13,14 +12,10 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo User: Administrator
 echo Pass: Tunghs123
 curl -O https://link.qthang.net/windows-rdp/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -L -o "C:\Users\Public\Desktop\butter\JJSploit.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/JJSploit%20Installer.exe > out.txt 2>&1
-curl -L -o "C:\Users\Public\Desktop\butter\roblox.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/RobloxPlayerLauncher%20(2).exe > out.txt 2>&1
-curl -L -o "C:\Users\Public\Desktop\butter\mukuro.txt" https://raw.githubusercontent.com/Greenn7838/Butter-s-awesome-repo/main/mukuro.txt > out.txt 2>&1
-curl -L -o "C:\Users\Public\Desktop\butter\protected.lua" https://raw.githubusercontent.com/Greenn7838/Butter-s-awesome-repo/main/Protected.lua > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\install.bat" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/install.bat > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
-sc start audiosrv >nul
-sc config Audiosrv start= auto >nul
+@REM sc start audiosrv >nul
+@REM sc config Audiosrv start= auto >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-ping -n 10 127.0.0.1 >nul
--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 @echo off
 md "C:\User\Public\ButterPack" > out.txt 2>&1
 
-curl -o "C:\User\Public\ButterPack\JJSploitInstaller.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/JJSploit%20Installer.exe
-curl -o "C:\User\Public\ButterPack\RobloxInstaller.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/RobloxPlayerLauncher%20(2).exe
-curl -o "C:\User\Public\ButterPack\mukuro.lua" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/mukuro.txt
-curl -o "C:\User\Public\ButterPack\protected.lua" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/Protected.lua
+curl -o "C:\User\Public\ButterPack\JJSploitInstaller.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/JJSploit%20Installer.exe > out.txt 2>&1
+curl -o "C:\User\Public\ButterPack\RobloxInstaller.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/RobloxPlayerLauncher%20(2).exe > out.txt 2>&1
+curl -o "C:\User\Public\ButterPack\mukuro.lua" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/mukuro.txt > out.txt 2>&1
+curl -o "C:\User\Public\ButterPack\protected.lua" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/Protected.lua > out.txt 2>&1
 
 ping 127.0.0.1 -t >nul
