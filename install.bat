e88778e34334ce7580feb8fba0f9cd6fefaa46c2
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-02-24 17:58:34 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,4 +2,5 @@
 md "C:\Users\Administrator\Desktop\ButterPack"
 cd C:\Users\Administrator\Desktop\ButterPack
 
-curl -o https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/pack.zip
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "curl -o pack.zip https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/pack.zip"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "unzip .\pack.zip"
