32ba38826504e8bc4cde9f1b7487d551d589606a
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-02-24 17:50:04 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,5 @@
 @echo off
-md "C:\User\Public\ButterPack" > out.txt 2>&1
+md "C:\Users\Administrator\Desktop\ButterPack"
+cd C:\Users\Administrator\Desktop\ButterPack
 
-curl -o "C:\User\Public\ButterPack\JJSploitInstaller.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/JJSploit%20Installer.exe > out.txt 2>&1
-curl -o "C:\User\Public\ButterPack\RobloxInstaller.exe" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/RobloxPlayerLauncher%20(2).exe > out.txt 2>&1
-curl -o "C:\User\Public\ButterPack\mukuro.lua" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/mukuro.txt > out.txt 2>&1
-curl -o "C:\User\Public\ButterPack\protected.lua" https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/Protected.lua > out.txt 2>&1
-
-ping 127.0.0.1 -t >nul
+curl -o https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/pack.zip
