4f9094335f44843e78ea81a0fc0acba09cc479d4
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-03-02 21:44:07 +0700

    Update check.bat

--- check.bat
+++ check.bat
@@ -2,6 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > out.txt 2>&1
 del /f "C:\Users\Public\Desktop\Firefox.lnk" > out.txt 2>&1
+md "C:\Users\Public\Desktop\MyApp" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user Administrator Tunghs123 /add >nul
@@ -11,6 +12,7 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo User: Administrator
 echo Pass: Tunghs123
 curl -O https://link.qthang.net/windows-rdp/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\MyApp\InstallApps.bat" https://raw.githubusercontent.com/Greenn7838/Butter-s-awesome-repo/main/install.bat > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 @REM sc start audiosrv >nul
