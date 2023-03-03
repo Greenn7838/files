3748b9d42e9a28f2ec5bc629085bee1005775577
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-03-03 16:57:39 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@ md "C:\Users\Public\Desktop\Pack"
 cd C:\Users\Public\Desktop\Pack
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "curl -o pack.zip https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/pack.zip"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "curl -o Trigon.zip https://github.com/Greenn7838/Butter-s-awesome-repo/raw/main/pack.zip"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "unzip .\pack.zip"
 
 del /f .\pack.zip
